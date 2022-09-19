54fa0275d51b31ab7eb57eb6e2819c1bee35bbb3
Denis <denis.selitsky@gmail.com>
2022-09-19 16:31:16 +0300

    feat: add class JukeBox

---
+++ JukeBox.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class JukeBox {
+    public static void main(String[] args) {
+        new JukeBox().go();
+    }
+
+    public void go() {
+
+    }
+}
