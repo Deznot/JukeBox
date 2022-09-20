eaf809080ad3b8d45d09cffe3b17372509fb1466
Denis <denis.selitsky@gmail.com>
2022-09-20 11:58:12 +0300

    feat: add method getSongs

--- JukeBox.java
+++ JukeBox.java
@@ -2,11 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class JukeBox {
+    ArrayList<String> songList = new ArrayList<String>();
     public static void main(String[] args) {
         new JukeBox().go();
     }
 
     public void go() {
-
+        getSongs();
+        System.out.println(songList);
     }
+
+    public void getSongs(){
+        try {
+            File file = new File("SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+        } catch(IOException ex) {
+            ex.printStackTrace();
+        }
+    };
 }
