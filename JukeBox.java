290928e05095ba16b1449a8d29a7a91ff0e71e65
Denis <denis.selitsky@gmail.com>
2022-09-20 14:23:49 +0300

    feat: add class Song

--- JukeBox.java
+++ JukeBox.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class JukeBox {
-    ArrayList<String> songList = new ArrayList<String>();
+    ArrayList<Song> songList = new ArrayList<Song>();
     public static void main(String[] args) {
         new JukeBox().go();
     }
@@ -10,14 +10,61 @@ public class JukeBox {
     public void go() {
         getSongs();
         System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
     }
 
     public void getSongs(){
         try {
             File file = new File("SongList.txt");
             BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+
         } catch(IOException ex) {
             ex.printStackTrace();
         }
     };
+
+    public void addSong(String lineToParse) {
+        String [] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
 }
+
+class Song {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    public Song(String t, String artist, String rating, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm(){
+        return bpm;
+    }
+
+    public String toString() {
+        return title;
+    }
+
+}   
